cdecf2d0f78a49dbc12bfcc4e03f2f1fd9be14ad
Parva Patel <parvapatel12@gmail.com>
2020-06-14 17:31:58 +0530

    Added links in buy.js

--- src/Buy.js
+++ src/Buy.js
@@ -59,7 +59,7 @@ export default class Buy extends React.Component {
       localStorage.setItem("salad", JSON.stringify(SALAD));
       localStorage.setItem("cheese", JSON.stringify(CHEESE));
       localStorage.setItem("cutlets", JSON.stringify(CUTLETS));
-      window.location.href = "/";
+      window.location.href = "/TheBurgerBank";
     } else {
       alert("Please enter your name to place the order.");
     }
