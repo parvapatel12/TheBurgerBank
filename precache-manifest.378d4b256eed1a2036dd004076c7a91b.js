222f96a86e0580ff8201dff42846325925f0d520
Patel <212797642@logon.ds.ge.com>
2020-06-14 15:57:59 +0530

    Updates

---
+++ precache-manifest.378d4b256eed1a2036dd004076c7a91b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf2ece3f446964d7507ae3776b44172",
+    "url": "/TheBurgerBank/index.html"
+  },
+  {
+    "revision": "e78d73e817d7e0a5e13f",
+    "url": "/TheBurgerBank/static/css/main.a69f9e01.chunk.css"
+  },
+  {
+    "revision": "bd8a82df6f7c58c2061c",
+    "url": "/TheBurgerBank/static/js/2.8f93a538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TheBurgerBank/static/js/2.8f93a538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78d73e817d7e0a5e13f",
+    "url": "/TheBurgerBank/static/js/main.a06c9468.chunk.js"
+  },
+  {
+    "revision": "e8427b00ff20e2dc59e8",
+    "url": "/TheBurgerBank/static/js/runtime-main.537573a4.js"
+  },
+  {
+    "revision": "ac2f550a2d13dc43f77927c6e638214a",
+    "url": "/TheBurgerBank/static/media/6.ac2f550a.jpg"
+  },
+  {
+    "revision": "af03d4cafdf90f21ce2a58f341c537a5",
+    "url": "/TheBurgerBank/static/media/7.af03d4ca.jpg"
+  },
+  {
+    "revision": "4d1d742cc664a5d530734ac5926990ff",
+    "url": "/TheBurgerBank/static/media/burger-background2.4d1d742c.jpg"
+  },
+  {
+    "revision": "b4dff14d81389b0ec7a4f2fc3719ffa9",
+    "url": "/TheBurgerBank/static/media/burger-background4.b4dff14d.png"
+  },
+  {
+    "revision": "9686fd5a6e9f3673611a8888da474dc3",
+    "url": "/TheBurgerBank/static/media/heading.9686fd5a.png"
+  }
+]);
